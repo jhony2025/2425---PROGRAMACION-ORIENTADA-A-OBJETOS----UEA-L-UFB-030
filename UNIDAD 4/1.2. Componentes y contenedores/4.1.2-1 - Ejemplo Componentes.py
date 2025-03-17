fccfffd8382b0a4d394722f3d23ff48e3dba3741
wrnunezz <wr.nunezz@uea.edu.ec>
2025-03-17 18:19:55 -0500

    Semana 14

--- UNIDAD 4/1.2. Componentes y contenedores/4.1.2-1 - Ejemplo Componentes.py
+++ UNIDAD 4/1.2. Componentes y contenedores/4.1.2-1 - Ejemplo Componentes.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, Menu, scrolledtext
 
-# Función que se ejecuta al presionar el botón.
+# Función que se ejecuta al presionar el botón
 def boton_accion():
     messagebox.showinfo("Acción", "Botón presionado")
 
